5b2f49afb58f1c8ed23f96a117d8b4f1f758221c
Karl Swedberg <kswedberg@gmail.com>
2015-06-29 10:00:41 -0400

    Ensure deploy:sync runs before deploy:build

--- gulpfile.js/tasks/deploy/index.js
+++ gulpfile.js/tasks/deploy/index.js
@@ -1,6 +1,10 @@
 var gulp = require('gulp');
+var runSequence = require('run-sequence');
 
-gulp.task('deploy', [
-  'deploy:sync',
-  'deploy:ping'
-]);
+gulp.task('deploy', function(cb) {
+  runSequence(
+    'deploy:sync',
+    'deploy:build',
+    cb
+  );
+});
