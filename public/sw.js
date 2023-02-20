e7d4fc42d30e7761bd799c3aee5c9153c713bbc0
Karl Swedberg <kswedberg@gmail.com>
2023-02-20 09:06:59 -0500

    Add Service Worker to delete all previous SW caches

---
+++ public/sw.js
@@ -0,0 +1,11 @@
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(cacheName) {
+          return caches.delete(cacheName);
+        })
+      );
+    })
+  );
+});
