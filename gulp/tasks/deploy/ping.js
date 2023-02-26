d0b008106e438ed500ade84df921d38bd8ca2c39
Karl Swedberg <kswedberg@gmail.com>
2023-02-26 16:28:46 -0500

    Remove unneeded files

--- gulp/tasks/deploy/ping.js
+++
@@ -1,8 +0,0 @@
-let gulp = require('gulp');
-let shell = require('gulp-shell');
-let cmd = `wget -O - -q -t 1 ${process.env.PING_URL}`;
-
-gulp.task('deploy:ping', () => {
-  return gulp.src('package.json', {read: false})
-  .pipe(shell(cmd));
-});
