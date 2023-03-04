936f0b3c02d7bc4381830eca5480c010fa446e12
Karl Swedberg <kswedberg@gmail.com>
2023-03-04 12:41:33 -0500

    Improve load comment script and add alternatives

--- src/assets/js/load-comments.js
+++ src/assets/js/load-comments.js
@@ -6,7 +6,9 @@
   const dsn = window.disqus_shortname || '';
   const firstScript = doc.getElementsByTagName('script')[0];
   const commentBtn = doc.getElementById('comment-btn');
+  const jsPreComment = doc.querySelector('.js-comments');
   const disqusWrapper = doc.getElementById('disqus_thread');
+
   const loadScript = function(id, url) {
     const scr = doc.createElement('script');
 
@@ -16,21 +18,22 @@
     firstScript.parentNode.insertBefore(scr, firstScript);
   };
 
-  commentBtn.classList.remove('hidden');
+  jsPreComment.classList.remove('hidden');
+
   const loadDisqus = function(event) {
     event.preventDefault();
+    // This is the loading indicator:
     const hid = disqusWrapper.querySelector('.hidden');
 
     if (hid) {
       hid.classList.remove('hidden');
     }
+
     loadScript('disqus-script', `https://${dsn}.disqus.com/embed.js`);
-    commentBtn.parentNode.removeChild(commentBtn);
+    jsPreComment.parentNode.removeChild(jsPreComment);
   };
 
   if (disqusWrapper && commentBtn) {
-
-
     commentBtn.addEventListener('click', loadDisqus);
   }
 
