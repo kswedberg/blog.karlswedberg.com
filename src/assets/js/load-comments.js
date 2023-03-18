1f923ecc266c7d538983884d19d70901b87795ec
Karl Swedberg <kswedberg@gmail.com>
2023-03-18 17:21:04 -0400

    Add share button to posts, using Web Share API

--- src/assets/js/load-comments.js
+++ src/assets/js/load-comments.js
@@ -37,4 +37,38 @@
     commentBtn.addEventListener('click', loadDisqus);
   }
 
+  const addShare = () => {
+    const tagsWrapper = document.querySelector('.tags-wrapper');
+    const canon = document.querySelector('link[rel="canonical"]');
+    const ogSiteName = document.querySelector('meta[property="og:site_name"]');
+    const ogTitle = document.querySelector('meta[property="og:title"]');
+    let shareData = {
+      title: ogSiteName.getAttribute('content'),
+      text: ogTitle.getAttribute('content'),
+      url: canon.getAttribute('href'),
+    };
+
+    if (!tagsWrapper || !navigator.canShare || !navigator.canShare(shareData)) {
+      return console.log(tagsWrapper);
+    }
+
+    const share = async() => {
+      try {
+        await navigator.share(shareData);
+      } catch (err) {
+        console.log(`Error: ${err}`);
+      }
+    };
+    const shareBtn = document.createElement('button');
+
+    shareBtn.type = 'button';
+    shareBtn.className = 'mt-8 px-2 py-0.5 rounded border text-sky-800 border-sky-800 dark:text-slate-200 dark:border-slate-200 hover:text-sky-900 dark:hover:text-white';
+    shareBtn.textContent = 'Share this post';
+
+    shareBtn.addEventListener('click', share, false);
+    tagsWrapper.insertAdjacentElement('afterend', shareBtn);
+  };
+
+  addShare();
+
 })(document);
