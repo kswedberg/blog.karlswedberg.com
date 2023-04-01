45547e94b719e2966f7d28e3f3a35dfe74b6dc55
Karl Swedberg <kswedberg@gmail.com>
2023-04-01 16:59:52 -0400

    More work on search

--- src/vue/site-search.vue
+++ src/vue/site-search.vue
@@ -1,41 +1,64 @@
 <template>
-  <form id="searchWrapper" action="https://good-search.karl.workers.dev/" class="relative z-10" @submit.prevent="onSubmit">
-    <label class="block" for="search-name">Search</label>
-    <input
-      v-model="searchName"
-      @input="onInput"
-      @keydown="moveSelection"
-      type="search"
-      name="name"
-      id="search-name"
-      class="p-1 border border-slate-600 rounded-md outline-none focus:ring focus:ring-blue-400"
-    >
-    <ul v-show="results.length && selectedIndex >= 0" class="absolute standard-color bg-white dark:bg-slate-900 mt-2 p-1 rounded-md border border-slate-200 dark:border-slate-700">
-      <SearchResult
-        v-for="(result, i) in results"
-        @activate="setIndex(i)"
-        :key="result.item.title + ' ' + (result.positions && result.positions.join())"
-        :item="result.item"
-        :positions="result.positions"
-        :selected="selectedIndex === i"
-      />
-    </ul>
-  </form>
+  <div class="flex items-center justify-end">
+    <form v-if="showForm" id="searchWrapper" action="https://good-search.karl.workers.dev/" class="search-form relative z-10" @submit.prevent="onSubmit">
+      <label :class="[
+        'search-label block absolute p-1 bg-white rounded-md leading-none mt-0.5 ml-px',
+        searchName.length && 'has-text',
+      ]" for="search-name">Search</label>
+      <input
+        v-model="searchName"
+        @input="onInput"
+        @keydown="moveSelection"
+        @focus="blurred = false"
+        @blur="clearResults"
+        type="search"
+        name="name"
+        ref="searchField"
+        id="search-name"
+        class="p-1 border border-slate-600 rounded-md outline-none focus:ring focus:ring-blue-400"
+      >
+      <ul v-show="results.length && selectedIndex >= 0" class="absolute standard-color text-left bg-white dark:bg-slate-900 mt-2 p-1 rounded-md border border-slate-200 dark:border-slate-700">
+        <SearchResult
+          v-for="(result, i) in results"
+          @activate="setIndex(i)"
+          :key="result.item.title + ' ' + (result.positions && result.positions.join())"
+          :item="result.item"
+          :positions="result.positions"
+          :selected="selectedIndex === i"
+        />
+      </ul>
+    </form>
+    <div class="inline-flex items-center">
+      <button @click="toggleForm" type="button" class="ml-1 p-1">
+        <svg v-if="!showForm" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+          <path d="M19.023 16.977c-.513-.488-1.004-.997-1.367-1.384-.372-.378-.596-.653-.596-.653l-2.8-1.337C15.34 12.37 16 10.763 16 9c0-3.859-3.14-7-7-7S2 5.141 2 9s3.14 7 7 7c1.763 0 3.37-.66 4.603-1.739l1.337 2.8s.275.224.653.596c.387.363.896.854 1.384 1.367l1.358 1.392.604.646 2.121-2.121-.646-.604c-.379-.372-.885-.866-1.391-1.36zM9 14c-2.757 0-5-2.243-5-5s2.243-5 5-5 5 2.243 5 5-2.243 5-5 5z"/>
+        </svg>
+        <svg v-else xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+          <path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"/>
+        </svg>
+
+      </button>
+    </div>
+  </div>
 </template>
 
 <script setup>
-import {ref} from 'vue';
+import {ref, nextTick} from 'vue';
 import {debounce} from '@/assets/js/debounce.mjs';
 import SearchResult from './search-result.vue';
 const selectedIndex = ref(0);
 const searchName = ref('');
+const searchField = ref(null);
 const results = ref([]);
+const blurred = ref(false);
+const showForm = ref(false);
 const responses = {};
 const baseUrl = 'https://good-search.karl.workers.dev/search';
 
 const clearResults = (event) => {
-  console.log(event.relatedTarget);
-  // results.value = [];
+  results.value = [];
+  selectedIndex.value = -1;
+  blurred.value = true;
 };
 const onSubmit = (event) => {
   const url = results.value?.[selectedIndex.value]?.item?.url;
@@ -44,6 +67,23 @@ const onSubmit = (event) => {
   }
 };
 
+const focusField = () => {
+  const searchField = document.getElementById('search-name');
+
+  if (!searchField) {
+    return console.log('could not find search field');
+  }
+  searchField.focus();
+};
+const toggleForm = async() => {
+  showForm.value = !showForm.value
+
+  await nextTick();
+
+  if (showForm.value && searchField.value) {
+    searchField.value.focus();
+  }
+};
 const moveUp = () => {
   if (selectedIndex.value < 0) {
     return;
@@ -77,6 +117,9 @@ const fetchResults = async() => {
     headers: {'Content-Type': 'application/json'}
   };
 
+  if (blurred.value) {
+    return;
+  }
   if (searchName.value.length < 2) {
     results.value = [{item: {title: 'Type 2 or more characters to search…'}}];
     return;
@@ -92,10 +135,28 @@ const fetchResults = async() => {
   if (!response.ok) {
     console.log('Response not ok');
   }
+
   const {data, metadata} = await response.json();
-  responses[searchName.value] = data;
-  results.value = data;
+  if (!blurred.value) {
+    responses[searchName.value] = data;
+    results.value = data;
+  }
+
 };
 
 const onInput = debounce(fetchResults, 200);
 </script>
+
+<style lang="postcss">
+.search-label {
+  transition: transform 0.2s;
+}
+
+.search-label:has(~ input:focus),
+.search-form:focus-within label,
+.search-label.has-text {
+  transform: scale(.8) translateY(-80%);
+  transform-origin: left;
+
+}
+</style>
