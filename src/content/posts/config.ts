8cf0c95b04e49ae17e38ba605f3e0bcf33b4c861
Karl Swedberg <kswedberg@gmail.com>
2023-10-14 15:35:16 -0400

    Lint ts file

--- src/content/posts/config.ts
+++ src/content/posts/config.ts
@@ -1,5 +1,6 @@
 // 1. Import utilities from `astro:content`
-import {z, defineCollection } from 'astro:content';
+import {z, defineCollection} from 'astro:content';
+
 // 2. Define the collection(s)
 const postCollection = defineCollection({
   schema: z.object({
@@ -7,10 +8,11 @@ const postCollection = defineCollection({
     title: z.string(),
     tags: z.array(z.string()),
     image: z.string().optional(),
-  })
+  }),
 });
+
 // 3. Export a single `collections` object to register the collection(s)
 //    This key should match the collection directory name in "src/content"
 export const collections = {
-  'posts': postCollection,
+  posts: postCollection,
 };
